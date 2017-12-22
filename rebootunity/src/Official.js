d20b78da8e763de53f98d6e75804ba61b8a8102d
Jan Humphries <jldennison@gmail.com>
2017-12-21 20:21:30 -0500

    update styles

--- rebootunity/src/Official.js
+++ rebootunity/src/Official.js
@@ -4,16 +4,16 @@ import ghost from './packman.png';
 class Official extends React.Component {  
   
   render() {
-    let classes = "official " + this.props.official.level + " " + this.props.official.party;
+    let classesOfficial = "official " + this.props.official.level + " " + this.props.official.party;
     let t = "Photo of " + this.props.official.name;
-    
+    let classesParty = "official-party official-detail " + this.props.official.party + "-label";
     return (
-      <li className={classes}>
+      <li className={classesOfficial}>
         <img className="official-photo" src={ghost} alt={t} />
         <div className="official-details">
           <p className="official-name official-detail">{this.props.official.name}</p>
           <p className="official-position official-detail">{this.props.official.position}</p>
-          <p className="official-party official-detail">{this.props.official.party}</p>
+          <p className={classesParty}>{this.props.official.party}</p>
           <p className="official-level official-detail">{this.props.official.level}</p>
         </div>
       </li>
