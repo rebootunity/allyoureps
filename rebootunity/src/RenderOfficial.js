708bfb988f6f7c8412862db663d339fdc4f70f8e
Jan Humphries <jldennison@gmail.com>
2017-12-31 15:18:12 -0500

    adds social channels to profile

--- rebootunity/src/RenderOfficial.js
+++ rebootunity/src/RenderOfficial.js
@@ -80,7 +80,7 @@ class RenderOfficial extends React.Component {
     if (url !== undefined && url !== '') {
       holder.push(
         <div key="url" className="official-detail weblink">
-          <a className="detail-link" href={url} title="Official website" target="_blank" rel="noopener noreferrer" >
+          <a className="detail-link subtitle" href={url} title="Official website" target="_blank" rel="noopener noreferrer" >
             <i className="material-icons">language</i>
             <span>{url}</span>
           </a>
@@ -97,6 +97,22 @@ class RenderOfficial extends React.Component {
                 <i className="material-icons">phone</i>
                 <span>{phone}</span></a>
             </div>);
+  }
+
+  renderSocial() {
+    let channels = this.props.official.channels !== undefined ? this.props.official.channels : undefined;
+    let holder = [];
+    if (channels !== undefined && channels !== '') {
+      channels.forEach((channel) =>{
+        let type = channel["type"],
+            id = channel["id"]
+        holder.push(
+          <a key={type} href={("http://" + type + ".com/" + id)} className={(type + " social-contact official-channel subtitle detail-link")} title="" target="_blank" rel="noopener noreferrer">
+            {type}
+          </a>)
+      })
+    }
+    return holder
   }
 
   handleClick() {
@@ -130,7 +146,9 @@ class RenderOfficial extends React.Component {
           {this.renderPhone()}
           {this.renderUrl()}
         </div>
-
+        <div key="social-div" className="usa-accordion-content panel official-social" aria-hidden={this.state['aria-hidden']}>
+          {this.renderSocial()}
+        </div>
       </li>
     );
   }
