b7ba0c52485ff4d2cb8274f82d0964cd68f89cfc
Jan Humphries <jldennison@gmail.com>
2018-01-15 11:44:42 -0500

    tweak site copy

--- allyourreps/src/SiteFooter.js
+++ allyourreps/src/SiteFooter.js
@@ -6,8 +6,8 @@ class SiteFooter extends React.Component {
       <footer className="footer">
         <div className="white-card content">
           <h2 className="title">About this site</h2>
-          <p>This is an open-source software project aimed at boosting political engagement among American citizens.</p>
-          <p>Technology has lagged behind in serving one of the most important groups in America: the people of America.</p><p>This site is an effort to modernize politics and make engagement as uncomplicated as possible.</p>
+          <p>This is an open-source software project focused on using technology to remove barriers to political engagement among American citizens.</p>
+          <p>See the <a href="https://github.com/rebootunity/allyoureps/tree/master/allyourreps" title="code repository for this project">GitHub repo</a> for more information. Built with <a href="https://reactjs.org/" title="React JS">ReactJS</a>.</p>
         </div>
         <small className="content caption">&copy; Copyright 2018 <a href="https://jannypie.github.io" target="_blank" rel="noopener noreferrer">Jan Dennison Humphries</a></small>
       </footer>
--- allyourreps/src/SiteIntro.js
+++ allyourreps/src/SiteIntro.js
@@ -4,16 +4,13 @@ class SiteIntro extends React.Component {
   render() {
     return (
       <section className="intro white-card content">
-        <h2 className="title"><em>all</em> levels of government, even local</h2>
+        <h2 className="title">all levels of government, even local</h2>
         <p><strong>All Your Reps</strong> takes the work out of finding your elected officials, so that you can focus on the issues.</p>
-        <p>Finding your elected officials requires visiting different websites, knowing district borders, and even what offices there are to fill. Often it feels like stumbling in the dark.</p>
-        <p>Here you can search for your elected representatives at all levels, all in one spot. You will find names, addresses, phone numbers, and social media accounts. Bookmark it, print it, send it to your email -- whatever works best for you!</p>
+        <p>Finding out who represents us can feel like a chore. Here you can search for your elected representatives at all levels, all in one spot. You will find names, addresses, phone numbers, and social media accounts. Bookmark it, print it, send it to your email -- whatever works best for you!</p>
         <h3 className="title">About your results</h3>
-        <p>Data is drawn from the <a href="https://developers.google.com/civic-information/" target="_blank" rel="noopener noreferrer" title="Google API website for developers">Google Civic Information API</a> in partnership with the <a href="http://votinginfoproject.org/" target="_blank" rel="noopener noreferrer" title="Voting Information Project website">Voting Information Project</a>.</p>
-        <p>Data for your results Results may include more than just name and office; some results include phone number, office address, website, and social media channels.</p>
-
+        <p>Data is drawn from the <a href="https://developers.google.com/civic-information/" target="_blank" rel="noopener noreferrer" title="Google API website for developers">Google Civic Information API</a> in partnership with the <a href="http://votinginfoproject.org/" target="_blank" rel="noopener noreferrer" title="Voting Information Project website">Voting Information Project</a>(VIP). Missing or incomplete positions may occur depending on completeness of data registered with the VPI. This website unfortunately cannot fill those holes at this time.</p>
         <h3 className="subhead">Find something wrong?</h3>
-        <p>Data is collected by the  drawn from Google&#8217;s API. More information can be found on their <a href="https://developers.google.com/civic-information/docs/ci_forum" target="_blank" rel="noopener noreferrer">support site.</a></p>
+        <p>Data is drawn from Google&#8217;s Civic Information API. More information may be found on their <a href="https://developers.google.com/civic-information/docs/ci_forum" target="_blank" rel="noopener noreferrer">support site.</a></p>
       </section>
     )
   }
